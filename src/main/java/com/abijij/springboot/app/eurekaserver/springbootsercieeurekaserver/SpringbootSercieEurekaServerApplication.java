212bc91d91a0d7ea50967378b58e7f1c927aab97
abijij <105397465+abijij@users.noreply.github.com>
2024-02-02 15:19:06 -0600

    first commit

---
+++ src/main/java/com/abijij/springboot/app/eurekaserver/springbootsercieeurekaserver/SpringbootSercieEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.abijij.springboot.app.eurekaserver.springbootsercieeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSercieEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSercieEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abijij/springboot/app/eurekaserver/springbootsercieeurekaserver/SpringbootSercieEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abijij.springboot.app.eurekaserver.springbootsercieeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSercieEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
